4c32943ba2a00f80919a19091ff81f006855145d
Bishal Maharjan <mrznalien1@gmail.com>
2025-07-07 15:53:35 +0545

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://mern-auth-theta-red.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
